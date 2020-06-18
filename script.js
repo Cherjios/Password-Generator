fc8487b931cfd88541edd46571c6218c7f0cde5c
cherjios <slopez0286@gmail.com>
2020-06-18 15:10:12 -0700

    Adding  variables to js

--- script.js
+++ script.js
@@ -7,8 +7,69 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Function to generate PassWord
+function generatePassword(){
+  var ConfirmSpecialCharactere = confirm("Do you want Special Characters included on your Password?");
+  var ConfirmUppercase = confirm("Do you want uppercase Characters included on your Password?");
+  var ConfirmLowercases = confirm("Do you want Lowercase Characters included on your Password?");
+  var ConfirmNumeric = confirm("Do you want Numeric Characters included on your Password?");
+  var lengthPassword = prompt("Select the length of your password between 8 and 128 Characters");
+  var validatelength = false;
+  var PasswordCharacteres = [];
+
+
+  // Declaring arrays to generate passwords 
+
+var SpecialCharacters = [" ", "!","\"","#", "$", "%", "&", "/'","(",")","*","+",",","-",".","/",":",";",
+"<","=",">", "?","@","[","\\","]","^","_","`","{","|","}","~"];
+
+var uppercase = ["Q","W","E","R","T","Y","U","I","O","P",
+"A","S","D","F","G","H","J","K","L",
+"Z","X","C","V","B","N","M"];
+
+var lowercase = uppercase.lowercase();
+
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  
+  
+  while(!validatelength){
+    if (lengthPassword >= 8 && lengthPassword <= 128){
+        if(ConfirmSpecialCharactere && ConfirmUppercase && ConfirmLowercases && ConfirmNumeric){
+          PasswordCharacteres = copyArray(SpecialCharacters);
+          PasswordCharacteres = copyArray(uppercase);
+          PasswordCharacteres = copyArray(lowercase);
+          PasswordCharacteres = copyArray(numeric);
+          
+
+        }
+
+
+
+    }
+    else{
+      alert("")
+  
+    }
+  }
+  
+  
+
+  
+
+
+}
+
+// Function to copy an array of 
+
+function copyArray (array){
+  var newArray = []
+  for(var i = 0; i < array.length; i++){
+    newArray[i] = array.push(i);
+  }
+  return newArray
+}
