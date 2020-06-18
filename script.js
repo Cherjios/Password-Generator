f81048218226ce506c46a62bd98588436a76d4af
cherjios <slopez0286@gmail.com>
2020-06-18 15:27:43 -0700

    adding more var

--- script.js
+++ script.js
@@ -25,29 +25,34 @@ function generatePassword(){
 
   // Declaring arrays to generate passwords 
 
-var SpecialCharacters = [" ", "!","\"","#", "$", "%", "&", "/'","(",")","*","+",",","-",".","/",":",";",
+  // 33 Characters 
+var SpecialCharacters = [" ","!","\"","#", "$", "%", "&", "/'","(",")","*","+",",","-",".","/",":",";",
 "<","=",">", "?","@","[","\\","]","^","_","`","{","|","}","~"];
 
+// 26 Characters
 var uppercase = ["Q","W","E","R","T","Y","U","I","O","P",
 "A","S","D","F","G","H","J","K","L",
 "Z","X","C","V","B","N","M"];
 
+// 26 Characters
 var lowercase = ["q","w","e","r","t","y","u","i","o","p",
 "a","s","d","f","g","h","j","k","l",
 "z","x","c","v","b","n","m"]
 
+// 10 characters 
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   
   
-  while(!validatelength){
+  
     if (lengthPassword >= 8 && lengthPassword <= 128){
         if(ConfirmSpecialCharactere && ConfirmUppercase && ConfirmLowercases && ConfirmNumeric){
           PasswordCharacteres = copyArray(SpecialCharacters);
           PasswordCharacteres = copyArray(uppercase);
           PasswordCharacteres = copyArray(lowercase);
           PasswordCharacteres = copyArray(numeric);
-          //Console.log the whole array to generate password
-          console.log(PasswordCharacteres);
+          // Console.log the whole array to generate password
+          // console.log(PasswordCharacteres);
+
 
         }
 
@@ -55,10 +60,9 @@ var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
     }
     else{
-      alert("")
+      alert("Length password between 8 and 128")
   
     }
-  }
   
   
 
@@ -75,4 +79,11 @@ function copyArray (array){
     newArray[i] = array.push(i);
   }
   return newArray
+}
+
+// Function to get random int 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
